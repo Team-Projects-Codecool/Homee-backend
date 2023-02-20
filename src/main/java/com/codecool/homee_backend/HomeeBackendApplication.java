1a8a4f2f8e503a33248c462326fafec6ffb27dac
damskw <damian.skwierawski@gmail.com>
2023-02-20 15:42:00 +0100

    Project base

---
+++ src/main/java/com/codecool/homee_backend/HomeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.homee_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeeBackendApplication.class, args);
+	}
+
+}
